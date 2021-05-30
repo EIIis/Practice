a5f4d007521884951afff4688721b4276f8edc12
Ellis Alcantara <ellisalcantara@gmail.com>
2021-05-30 12:14:09 -0500

    Class Photos added

---
+++ classPhotos.py
@@ -0,0 +1,15 @@
+# https://www.algoexpert.io/questions/Class%20Photos
+def classPhotos(redShirtHeights, blueShirtHeights):
+    redShirtHeights.sort()
+	blueShirtHeights.sort()
+	
+	if redShirtHeights[0] > blueShirtHeights[0]:
+		for i in range(len(redShirtHeights)):
+			if redShirtHeights[i] <= blueShirtHeights[i]:
+				return False
+		return True
+	else:
+		for i in range(len(blueShirtHeights)):
+			if blueShirtHeights[i] <= redShirtHeights[i]:
+				return False
+		return True
