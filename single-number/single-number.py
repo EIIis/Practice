6f07f35459819d8ed37fce803afcaa3c0e7563d3
Ellis Alcantara <47289830+EIIis@users.noreply.github.com>
2021-08-12 17:15:11 -0500

    Update single-number.py

--- single-number/single-number.py
+++ single-number/single-number.py
@@ -5,22 +5,14 @@ class Solution(object):
         :rtype: int
         """
         seen = {}
-        if len(nums) == 1:
-            return nums[0]
-        else:
-            for i in nums:
-                if i in seen:
-                    seen[i] += 1
-                else:
-                    seen[i] = 1
+        
+        for i in nums:
+            if i in seen:
+                seen[i] += 1
+            else:
+                seen[i] = 1
         
           
         for key, value in seen.items() :
             if value == 1:
                 return key
-
-    
-
-                
-        
-        
