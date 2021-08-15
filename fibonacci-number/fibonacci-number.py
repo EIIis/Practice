6e8d39ee28e918624e22cb20219a22607f884465
Ellis Alcantara <47289830+EIIis@users.noreply.github.com>
2021-08-15 18:24:25 -0500

    Runtime: 8 ms, faster than 99.83% of Python online submissions for Fibonacci Number. Memory Usage: 13.4 MB, less than 65.77% of Python online submissions for Fibonacci Number.

--- fibonacci-number/fibonacci-number.py
+++ fibonacci-number/fibonacci-number.py
@@ -10,4 +10,17 @@ class Solution(object):
 		    return 1
         else:
             return self.fib(n - 1) + self.fib(n - 2)
+
+class Solution(object):
+    def fib(self, n,  memo={0: 0, 1: 1}):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n in memo:
+		    return memo[n]
+        else:
+		    memo[n] = self.fib(n - 1, memo) + self.fib(n - 2, memo)
+		    return memo[n]
+
         
