194864b1cf005f16ebe5385e6b2b9e736337ae9f
Ellis Alcantara <47289830+EIIis@users.noreply.github.com>
2021-08-23 12:54:33 -0500

    Time: 60 ms (44.38%), Space: 13.3 MB (66.05%) - LeetHub

--- palindrome-number/palindrome-number.py
+++ palindrome-number/palindrome-number.py
@@ -4,17 +4,16 @@ class Solution(object):
         :type x: int
         :rtype: bool
         """
-        x = str(x) # Converting the int to a str
-        right = len(x) - 1 # Creating an end pointer
-        left = 0 # Creating a starting pointer
         
-        while left < right: # Creating a while loop to contiuiously run
-            for i in x: # iterating through the string
-                if x[left] != x[right]: # Checking to see if the elements are the same, when on opposite sides
-                    return False # If they're not, return False
-                else: # If they are we increase/decrease the pointers by one
-                    left += 1
-                    right -= 1
-        return True # If it continues until the while loop is statified we return True
+        x = str(x)
+        left = 0
+        right = len(x) - 1
         
+        while left < right:
+            for idx in x:
+                if x[left] != x[right]:
+                    return False
+                left += 1
+                right -= 1
+        return True 
         
