35fc4db54833167b4a37a42b343bb66e2a8ad3ce
Ellis Alcantara <47289830+EIIis@users.noreply.github.com>
2021-09-05 10:16:12 -0500

    Time: 116 ms (49.32%), Space: 14.5 MB (90.41%) - LeetHub

--- missing-number/missing-number.py
+++ missing-number/missing-number.py
@@ -4,17 +4,19 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        
         nums.sort()
         nextVal = nums[0]
         
         if nextVal != 0:
             return 0
         else:
-            for i in nums:
-                if i == nextVal:
+            for num in nums:
+                if num == nextVal:
                     nextVal += 1
                 else:
                     return nextVal
-            length = len(nums) - 1
-            last = nums[length] + 1
+                
+            last = nums[len(nums) - 1] + 1
+            
             return last
