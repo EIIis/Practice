ccc7961daa166f850262c8fa864963d557a083eb
Ellis Alcantara <47289830+EIIis@users.noreply.github.com>
2021-09-15 12:49:16 -0500

    Time: 358 ms (9.22%), Space: 15.5 MB (74.30%) - LeetHub

--- squares-of-a-sorted-array/squares-of-a-sorted-array.py
+++ squares-of-a-sorted-array/squares-of-a-sorted-array.py
@@ -4,7 +4,27 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
+        
+        results = [0] * len(nums)
+        
+        left = 0
+        right = len(nums) - 1
+        
+        
+        for i in range(len(nums) - 1, -1, -1):
+            if abs(nums[left]) < abs(nums[right]):
+                results[i] = nums[right] ** 2
+                right -= 1
+            else:
+                results[i] = nums[left] ** 2
+                left += 1
+                
+        return results    
+            
+        
+        """
         arr = []
+        
         for i in nums:
             sqrNum = i ** 2
             arr.append(sqrNum)
@@ -12,5 +32,4 @@ class Solution(object):
         arr.sort()
         
         return arr
-            
-        
+        """
