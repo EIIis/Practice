fff3a10fbd3c7d6266da44542a07016332be7e90
Ellis Alcantara <47289830+EIIis@users.noreply.github.com>
2021-10-29 13:29:02 -0500

    Time: 12 ms (95.79%), Space: 13.5 MB (36.46%) - LeetHub

--- climbing-stairs/climbing-stairs.py
+++ climbing-stairs/climbing-stairs.py
@@ -7,7 +7,7 @@ class Solution(object):
         one = 1
         two = 1
         
-        for i in range(n - 1):
+        for _ in range(n - 1):
             temp = one
             one += two
             two = temp
